a3542aca26df3cfc57b4b230d52c9fec9bd6bbf7
Tommy Parnell <parnell.tommy@hotmail.com>
2014-05-23 19:51:13 -0400

    adding unit tests and NDepend

---
+++ src/NOCQ.UnitTest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOCQ.UnitTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        public static void Success()
+        {
+            Assert.Pass();
+        }
+    }
+}
