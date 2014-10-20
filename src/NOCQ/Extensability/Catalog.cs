605a62636481c8f33d71e712df2dc3e8b46bef87
Tommy Parnell <tparnell8@gmail.com>
2014-10-20 16:24:02 -0400

    load all in current dir

--- src/NOCQ/Extensability/Catalog.cs
+++ src/NOCQ/Extensability/Catalog.cs
@@ -6,9 +6,9 @@ namespace NOCQ.Extensability
 {
     public class Catalog
     {
-		//static DirectoryCatalog dcatalog = new DirectoryCatalog("plugins", "*.dll");
+		static DirectoryCatalog dcatalog = new DirectoryCatalog(".", "*.dll");
         static AssemblyCatalog acatalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
-		private static AggregateCatalog Cat = new AggregateCatalog(acatalog);
+		private static AggregateCatalog Cat = new AggregateCatalog(acatalog,dcatalog);
 		private static CompositionContainer _container = new CompositionContainer(Cat);
 		public CompositionContainer Container { get { return _container; } }
 		public Catalog()
